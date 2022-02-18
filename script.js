3b645a25e5f0503d9950a851afa52237bba3bd96
unknown <finjoy3@gmail.com>
2022-02-18 22:37:34 +0300

    v2. Фавикон, карточки через html, исправлен попап

--- script.js
+++ script.js
@@ -1,87 +1,29 @@
-// class Cat {
-//     constructor(name, img_link, age, rate, favourite, description, id){
-//         this.name = name;
-//         this.img_link = img_link;
-//         this.age = age;
-//         this.rate = rate;
-//         this.favourite = favourite;
-//         this.description = description;
-//         this.id = id;
-//     }
-// };
-
-// let catContent = "";
-// cats.forEach(function(item){
-//     let rate = item.rate;
-//     catContent += `
-//         <div class="card_cat">
-//             <div class="cat-img" style="background-image: url(${item.img_link})"></div>
-//             <h3>${item.name}</h3>
-//             <p class="rate">
-//                 ${rate}
-//             </p>
-//         </div>
-//     `
-// });
-    
-// document.querySelector('main').innerHTML += catContent;
-
-// cats.forEach(function(item){
-//     let tagRate = document.querySelector('.rate');
-//     let rate = item.rate;
-//     console.log(tagRate);
-//     console.log(rate);
-    // for (let i=1; i<=rate; i++){
-    //     let catRate = document.createElement("img");
-    //     catRate.src = "img/cat-fill.svg";
-    //     catRate.alt= "^_^";
-    //     // console.log(999);
-    //     // console.log(tagRate);
-    //     // tagRate.appendChild(catRate);
-    // };    
-// });
-
-
 const infoBlock = document.querySelector(".popup");
 const main = document.querySelector('main');
 
-cats.forEach(function(item){
-    let rate = item.rate; 
-
-    let cardCat =  document.createElement("div");
-    cardCat.className = 'card_cat';
-    main.append(cardCat);
-
-    let catImg = document.createElement("div");
-    catImg.className = "cat-img";
-    catImg.style.backgroundImage = `url(${item.img_link})`;
-    cardCat.append(catImg);
-
-    let catName = document.createElement("h3");
-    catName.innerHTML = item.name;
-    cardCat.append(catName);
-
-    let catRate = document.createElement("p");
-    catRate.className = 'rate';
-    cardCat.append(catRate);
-
-    for (let i=1; i<=rate; i++){
-        let catRateImg = document.createElement("img");
-        catRateImg.className = "rate-img";
-        catRateImg.src = "img/cat-fill.svg";
-        catRateImg.alt= "^_^";
-        catRate.append(catRateImg);        
+const rate = function(numberRate){
+    let noRate = 10-numberRate;
+    let count = '';
+    for (let i=1; i<=numberRate; i++){
+        count += '<img class="rate-img" src="img/cat-fill.svg" alt="^_^">';        
     };
-    let noRate = 10-rate;
-        if (noRate >= 1) {
-            for (let i=1; i<=noRate; i++){
-                let catNoRateImg = document.createElement("img");
-                catNoRateImg.className = "rate-img";
-                catNoRateImg.src = "img/cat-stroke.svg";
-                catNoRateImg.alt= "o_o";
-                catRate.append(catNoRateImg);
-            } 
-        }
+    if (noRate >= 1) {
+        for (let i=1; i<=noRate; i++){
+            count += '<img class="rate-img" src="img/cat-stroke.svg" alt=o_o">'
+        };
+    };
+    return count;   
+};
+
+cats.forEach(function(item){
+    const itemCat = `
+        <div class="card_cat">
+            <div class="cat-img" style="background-image: url(${item.img_link})"></div>
+            <h3>${item.name}</h3>
+            <p class="rate">${rate(item.rate)}</p> 
+        </div>
+    `;
+    main.innerHTML += itemCat;
 });
 
 const getWord = function (year, w1, w2, w0) {
@@ -123,3 +65,44 @@ for (let i = 0; i < cards.length; i++) {
         showPopup(cats[i]);
     })
 };
+
+
+
+//альтернатиынй способ (для себя)
+// cats.forEach(function(item){
+    // let cardCat =  document.createElement("div");
+    // cardCat.className = 'card_cat';
+    // main.append(cardCat);
+
+    // let catImg = document.createElement("div");
+    // catImg.className = "cat-img";
+    // catImg.style.backgroundImage = `url(${item.img_link})`;
+    // cardCat.append(catImg);
+
+    // let catName = document.createElement("h3");
+    // catName.innerHTML = item.name;
+    // cardCat.append(catName);
+
+    // let catRate = document.createElement("p");
+    // catRate.className = 'rate';
+    // cardCat.append(catRate);
+
+    // let rate = item.rate; 
+    // for (let i=1; i<=rate; i++){
+    //     let catRateImg = document.createElement("img");
+    //     catRateImg.className = "rate-img";
+    //     catRateImg.src = "img/cat-fill.svg";
+    //     catRateImg.alt= "^_^";
+    //     catRate.append(catRateImg);        
+    // };
+    // let noRate = 10-rate;
+    //     if (noRate >= 1) {
+    //         for (let i=1; i<=noRate; i++){
+    //             let catNoRateImg = document.createElement("img");
+    //             catNoRateImg.className = "rate-img";
+    //             catNoRateImg.src = "img/cat-stroke.svg";
+    //             catNoRateImg.alt= "o_o";
+    //             catRate.append(catNoRateImg);
+    //         } 
+    //     }
+// });
